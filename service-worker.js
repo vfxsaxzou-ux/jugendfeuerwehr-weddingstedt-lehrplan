0e5d92ed180816975c64c9ccef2f72133a52bbb2
vfxsaxzou-ux <vfxsaxzou@gmail.com>
2026-09-03 05:06:39 +0200

    Fahrzeugkunde wieder standardmäßig ausblenden und Cache aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jf-lehrplan-v1";
+const CACHE_NAME = "jf-lehrplan-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
